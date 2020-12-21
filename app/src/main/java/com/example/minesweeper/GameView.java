ef57f2f48af833dc75887787218adce92de0ce2e
VeitAl <veit_alberding@gmx.de>
2020-12-21 13:46:13 +0100

    Initial commit + milestone #1

---
+++ app/src/main/java/com/example/minesweeper/GameView.java
@@ -0,0 +1,22 @@
+package com.example.minesweeper;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
