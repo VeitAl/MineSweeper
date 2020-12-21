995ec83758aa14a0dd5643efce94a051b8f517ca
VeitAl <veit_alberding@gmx.de>
2020-12-21 15:08:19 +0100

    milestone #2

--- app/src/main/java/com/example/minesweeper/GameView.java
+++ app/src/main/java/com/example/minesweeper/GameView.java
@@ -1,12 +1,19 @@
 package com.example.minesweeper;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    Rect field;
+
+    Paint covered, uncovered, flag;
 
     public GameView(Context context) {
         super(context);
@@ -18,5 +25,36 @@ public class GameView extends View {
 
     public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        setBackgroundColor(Color.WHITE);
+        canvas.save();
+        canvas.translate(0,0);
+
+        int fieldLength = (getWidth()/10);
+
+        covered = new Paint();
+        covered.setStyle(Paint.Style.FILL);
+        covered.setColor(Color.BLACK);
+
+        field = new Rect(5, 5, fieldLength, fieldLength);
+
+
+        for(int i = 0; i < 10; i++) {
+            canvas.save();
+
+            for(int j = 0; j < 10; j++){
+                canvas.save();
+                canvas.translate( (i * fieldLength), (j * fieldLength));
+                canvas.drawRect(field, covered);
+                canvas.restore();
+            }
+            canvas.restore();
+        }
     }
 }
